387054b0d2b93ea18a25cfae22891af978b0b676
Sydney Smith <smithsydneyn@gmail.com>
2026-06-24 14:02:49 -0600

    Land use investigation

--- skin_temp.py
+++ skin_temp.py
@@ -31,8 +31,9 @@ import from_savanna.nclcmaps as cmap
 from preprocessor.data_access import get_data
 
 
-noise = get_data('TSK', 5, 'd01', 'noise')
-ctl = get_data('TSK', 5, 'd01', 'ctl')
+# %%
+noise = get_data('TSK', 5, 'd03', 'noise')
+ctl = get_data('TSK', 5, 'd03', 'ctl')
 
 # calculate anomaly skin temperature over d01 between noise experiment and ctl
 # note we want this to be close to 0
@@ -55,13 +56,14 @@ flat_axs = axs.flatten()
 labels = ['a.', 'b.', 'c.']
 
 # open WRF to pull elevation data
-open_elevation = Dataset(current_file_directory / 'wrfout' / f'wrfout_ctl' / '2016' / f'wrfout_d01_2016-05-01_00:00:00')
+open_elevation = Dataset(current_file_directory / 'wrfout' / f'wrfout_ctl' / '2016' / f'wrfout_d03_2016-05-01_00:00:00')
 # use getvar to access data
 elevation_data = getvar(open_elevation, 'ter')
 elevation_lat, elevation_lon = latlon_coords(elevation_data)
 
 # set elevation bands to appear every 500 meters
 elevation_levels = np.arange(0, 8500, 500)
+
 
 for ax, exp, label in zip(flat_axs, [noise_data, ctl_data, anom_data], labels):
     if label == 'c.':
@@ -97,10 +99,13 @@ for ax, exp, label in zip(flat_axs, [noise_data, ctl_data, anom_data], labels):
     # colorbar function passed using the scalar mappable 
     cbar = fig.colorbar(sm, ax = ax, orientation = 'horizontal', extend = 'both', ticks = levels, boundaries = levels, aspect = 30, location = 'bottom', pad = 0.015, format = cbar_format)
     cbar.ax.tick_params(labelsize = 10)
-    
 
+   
+
+    
+# %%
 # check out land classifications of red dots
-domain = 'd01'
+domain = 'd03'
 fpath = current_file_directory.parent.parent / 'husile' / 'karakoram' / 'model_result' / 'wps_k2' / f'geo_em.{domain}.nc'
 
 wrf_file = Dataset(fpath)
@@ -109,17 +114,17 @@ land_use = getvar(wrf_file, 'LU_INDEX')
 fig = plt.figure(figsize = (10, 8))
 ax = plt.axes(projection = ccrs.PlateCarree())
 
-# see land use categories here: https://www2.mmm.ucar.edu/wrf/users/docs/user_guide_V3/user_guide_V3.3/users_guide_chap3.htm#_Land_Use_and
+# see land use categories here: https://www.scielo.org.mx/scielo.php?script=sci_arttext&pid=S1405-55462023000200435
 # create custom color map for land use categories
 colors = plt.cm.jet(np.linspace(0, 1, 25))
-# call out number 16 as black for water bodies
-colors[20] = [0, 0, 0, 1] 
+# call out number 21 as black for water bodies (MODIS WRF land use classifications)
+colors[21] = [0, 0, 0, 1] 
 cmap = mcolors.ListedColormap(colors)
 norm = mcolors.BoundaryNorm(np.arange(0, 26, 1), cmap.N)
 
 # plot land use data
-contour = ax.contourf(land_use['XLONG_M'], land_use['XLAT_M'], land_use.values, levels = np.arange(0, 25, 1), cmap = cmap, norm = norm, transform = ccrs.PlateCarree())
-plt.colorbar(contour, ax = ax,label = 'Land Use Classification', ticks = np.arange(0, 25, 1), orientation = 'horizontal', pad = 0.05)
+mesh = ax.pcolormesh(land_use['XLONG_M'], land_use['XLAT_M'], land_use.values, cmap = cmap, norm = norm, transform = ccrs.PlateCarree())
+plt.colorbar(mesh, ax = ax,label = 'Land Use Classification', ticks = np.arange(0, 25, 1), orientation = 'horizontal', pad = 0.05)
 plt.show()
 
 
